af109667344000c8c5d92df2e5a6fe625b50df61
KaruzG <josemiguelcpcs@gmail.com>
2022-08-09 20:21:23 +0200

    Have draggable notes Now needs to set to top of zindex

--- draggable.js
+++ draggable.js
@@ -1,5 +1,17 @@
-dragElement(document.getElementsByClassName("draggableNote"));
+const notes = document.querySelectorAll(".draggableNote");
 
-function dragElement(element) {
+notes.forEach(note => {
+    const drag = (e) => {
+        note.style.top = e.pageY + "px";
+        note.style.left = e.pageX + "px";
+        note.style.zindex = 1;
+    }
     
-}
+    note.addEventListener("mousedown", () => {
+        window.addEventListener("mousemove" , drag);
+    });
+    
+    window.addEventListener("mouseup", () => {
+        window.removeEventListener("mousemove" , drag);
+    }); 
+});
