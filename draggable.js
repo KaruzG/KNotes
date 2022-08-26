dbb8026c786c30bdf5af6065ac5440068d85a188
KaruzG <josemiguelcpcs@gmail.com>
2022-08-26 19:07:36 +0200

    zIndex now works properly

--- draggable.js
+++ draggable.js
@@ -1,4 +1,8 @@
 var draggableElements = document.getElementsByClassName("draggableNote");
+var notes = document.querySelectorAll(".draggableNote");
+var lastSelectedNote = null;
+
+console.log(draggableElements.length)
 
 for(var i = 0; i < draggableElements.length; i++){
     dragElement(draggableElements[i]);
@@ -14,6 +18,26 @@ function dragElement(elmnt) {
         pos4 = parseInt(e.clientY);
         document.onmouseup = closeDragElement;
         document.onmousemove = elementDrag;
+        
+        // zIndex changer
+        if (lastSelectedNote != elmnt) { // If the same note is clicked multiple times, doesnt need to do anything with zIndex
+            elmnt.style.zIndex = draggableElements.length + 1; // Set the zIndex to the top
+            
+            console.log("⚠️   " + elmnt.id + " selected!");            // 
+            if(lastSelectedNote != null) {                             //       DEBUG THINGS
+                console.log(lastSelectedNote.id + " ES LA DE ANTES");  //
+            }
+
+            notes.forEach(function(note){   // -1 to every note but cant go below 0
+                if (note.style.zIndex != 0) {
+                    note.style.zIndex = note.style.zIndex - 1;   
+                }
+                console.log(note.id + " " + note.style.zIndex);
+            });
+    
+            lastSelectedNote = elmnt;   // Save for later the selected note
+            console.log(lastSelectedNote.id + "ES LA NUEVA");
+        }
         return false;
     }
 
